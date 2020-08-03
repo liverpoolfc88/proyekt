b2341aa34ea42d0e56f8fe1ff7a8860a324189b6
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-08-03 17:18:10 +0500

    sas
    asa

--- backend/app/File.php
+++ backend/app/File.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-   protected $fillable = ['name','ext','mime','size'];
+    protected $table = 'files';
+    protected $fillable = ['name','ext','mime','size'];
 }
 
